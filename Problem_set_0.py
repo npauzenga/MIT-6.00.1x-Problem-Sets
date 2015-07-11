b4c19abc8af0c69224395105b6aa898f2f031546
Nate Pauzenga <npauzenga@gmail.com>
2015-07-11 14:14:21 -0400

    restructured dirs

--- Problem_set_0.py
+++
@@ -1,7 +0,0 @@
-dob = str(raw_input("Please enter your year of birth: "))
-
-if(dob >= 1990) :
-    print("child")
-    
-lastName = str(raw_input("Please enter your last name: "))
-print(lastName + " " + dob)
